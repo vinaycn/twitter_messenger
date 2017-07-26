eac887063e206e3158c7ea1c68eb09f3d9ff30a3
vinaycn <vinaykumarcn25@gmail.com>
2017-07-26 15:14:10 -0400

    Enabled CORS origin request in Spring Security

--- src/main/java/org/twitter/messenger/configuration/SecurityConfiguration.java
+++ src/main/java/org/twitter/messenger/configuration/SecurityConfiguration.java
@@ -1,11 +1,17 @@
 package org.twitter.messenger.configuration;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -20,16 +26,30 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/h2-console/*").permitAll().anyRequest().authenticated().and()
+		http.cors()
+		.and().authorizeRequests().antMatchers("/h2-console/*").permitAll().anyRequest().authenticated().and()
 				.httpBasic().authenticationEntryPoint(myBasicAuthenticationEntryPoint);
 
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
 	}
 
+	
+	
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser(username).password(password).roles("USER");
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080/twitterapp/"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 	
 	
